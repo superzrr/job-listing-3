e2079d1ed8c394ddf270c4d097bc1715d4bedcda
superzrr <superzrr@gmail.com>
2017-04-25 13:54:57 +0800

    implement step1

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,5 @@
 module ApplicationHelper
+  def glyph(icon_name_postfix, hash={})
+    content_tag :span, nil, hash.merge(class: "fa fa-#{icon_name_postfix.to_s.gsub('_','-')} aria-hidden='true'")
+  end
 end
